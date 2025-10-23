d2b9d1ff8a2f9f828b6ee71b99de02e0a06606a9
John Doe <johndoe@example.com>
2025-10-23 21:57:56 +0300

    Added a class

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() {}
 
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
